e79b01112d804331535171339709c9b0c0977f79
catsigma <im@catsigma.com>
2017-10-16 11:38:53 +0800

    add client

---
+++ client/js/index.js
@@ -0,0 +1,29 @@
+(function(window, document){
+  var util = {
+    post: function(url, params, callback){
+      var xhr = new XMLHttpRequest()
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          callback(this.responseText)
+        }
+      }
+      xhr.open("POST", url, true)
+      xhr.send(JSON.stringify(params))
+    }
+  }
+
+  var domain = 'http://127.0.0.1:8080'
+  var rpcCall = function(method, params, callback){
+    var router = {
+      'bootstrapped': '/bootstrapped',
+      'blocks': '/blocks'
+    }
+
+    util.post(domain + router[method], params, callback)
+  }
+
+  window.BTW = {
+    domain: domain,
+    rpcCall: rpcCall    
+  }
+})(window, document)
